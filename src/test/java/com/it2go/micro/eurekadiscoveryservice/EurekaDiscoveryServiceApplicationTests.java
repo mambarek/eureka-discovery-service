780d32d431c28057622b4d2fceaa25a898692ca2
Mohamed Ali Mbarek <mbarek@it-2go.de>
2021-01-19 00:24:14 +0100

    first checkin

---
+++ src/test/java/com/it2go/micro/eurekadiscoveryservice/EurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.it2go.micro.eurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaDiscoveryServiceApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
